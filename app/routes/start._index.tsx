e04951c6d2f1b53dc5c19066672362a08212e65b
Sarah Ransohoff <sranso@gmail.com>
2023-11-22 23:39:25 -0500

    weird start

--- app/routes/start._index.tsx
+++ app/routes/start._index.tsx
@@ -1,7 +1,54 @@
 export default function Start() {
   return (
     <main>
-      <h1>Start</h1>
+      <img src='/savannah.gif' className='w-full' />
+      <div className='font-mono bg-sky-200 h-screen'>
+        <div className='text-center'>
+          <p className='whitespace-nowrap truncate text-lime-200 py-4 bg-yellow-900'>
+            regret everything about that weekend. she regrets almost everything
+            about that weekend. i
+          </p>
+          <div className='bg-amber-50 relative overflow-hidden'>
+            <div className='mx-auto text-blue-50 w-fit bg-emerald-900 border border-yellow-100'>
+              <p className='pl-6'>I</p>
+              <p className='pl-5'>I</p>
+              <p className='pl-5'>I</p>
+              <p className='pl-4'>I</p>
+              <p className='pl-3'>I</p>
+              <p className='pl-3'>I</p>
+              <p className='pl-2'>I</p>
+            </div>
+            <div className='bg-lime-900 absolute h-4/6 w-20 right-28 top-0 text-cyan-100 text-xs whitespace-nowrap space-y-6 [&>p]:-ml-20'>
+              <p>
+                I ate broccoli for dinner. The potatoes were especially nice
+              </p>
+              <p>We should have more parades</p>
+              <p>when do you retire your friends</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <span>&plusmn;</span>
+          <span>&permil;</span>
+          <span>&theta;</span>
+          <span>&Delta;</span>
+          <span>&hArr;</span>
+          <span>&frac12;</span>
+          <span>&divide; see divide quote alan watts</span>
+          <span>&aelig;</span>
+          <span>&hearts;</span>
+        </div>
+        <div className='fixed bottom-0 text-lime-200 text-sm [&>p]:w-fit [&>p]:bg-emerald-900'>
+          <p className=''>she wasn't herself. please remember</p>
+          <p>that we nurse and grow our wounds even as</p>
+          <p>they hold our eyes, pleading</p>
+          <p className='italic'>
+            leave me here. we have no more good left to give.
+          </p>
+          <p>but they are certain, and you are not.</p>
+          <p>we evolved to eliminate uncertainty.</p>
+        </div>
+      </div>
     </main>
   );
 }
