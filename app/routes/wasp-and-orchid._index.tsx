5172c5a83a49bccc88c4ea9a3a0abd97880bc40f
Sarah Ransohoff <sranso@gmail.com>
2023-11-24 18:26:28 -0500

    More format

--- app/routes/wasp-and-orchid._index.tsx
+++ app/routes/wasp-and-orchid._index.tsx
@@ -1,3 +1,5 @@
+import { Link } from '@remix-run/react';
+
 export default function WaspAndOrchid() {
   const iframeOverrides = `
     .ytp-chrome-top, .ytp-show-cards-title, .ytp-impression-link {
@@ -6,12 +8,26 @@ export default function WaspAndOrchid() {
   `;
   return (
     <div className='flex w-screen h-screen relative'>
-      <p className='italic absolute bottom-10 left-6 w-full md:w-8/12'>
-        The orchid deterritorializes by forming an image, a tracing of a wasp;
-        but the wasp reterritorializes on that image. The wasp is nevertheless
-        derritorialized, becoming a piece in the orchid's reproductive
-        apparatus. But it reterritorializes the orchid by transporting its
-        pollen. Wasp and orchid, as heterogeneous elements, form a rhizome.
+      <p className='absolute bottom-2 left-2 w-full md:w-8/12 bg-emerald-950 p-4 text-orange-100'>
+        <blockquote cite='https://files.libcom.org/files/A%20Thousand%20Plateaus.pdf'>
+          The orchid deterritorializes by forming an image, a tracing of a wasp;
+          but the wasp reterritorializes on that image. The wasp is nevertheless
+          derritorialized, becoming a piece in the orchid's reproductive
+          apparatus. But it reterritorializes the orchid by transporting its
+          pollen. Wasp and orchid, as heterogeneous elements, form a rhizome.
+        </blockquote>
+        <br />
+        &mdash;{' '}
+        <Link
+          to='https://files.libcom.org/files/A%20Thousand%20Plateaus.pdf'
+          target='_blank'
+        >
+          <span className='underline'>
+            A Thousand Plateaus: Capitalism and Schizophrenia
+          </span>
+        </Link>{' '}
+        by the French philosopher Gilles Deleuze and the French psychoanalyst
+        Félix Guattari
       </p>
       <style>{iframeOverrides}</style>
       <iframe
