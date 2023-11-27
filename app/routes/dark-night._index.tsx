ec342682f1d4671eec6e3493b25a1dde216c440b
Sarah Ransohoff <sranso@gmail.com>
2023-11-27 10:26:25 -0500

    Better lights

--- app/routes/dark-night._index.tsx
+++ app/routes/dark-night._index.tsx
@@ -14,13 +14,14 @@ export default function DarkNight() {
     'h-3 w-3',
     'h-3.5 w-3.5',
   ];
-  const COLORS = ['bg-cyan-900', 'bg-emerald-50'];
+  const COLORS = ['bg-cyan-900', 'bg-emerald-50', 'bg-rose-300'];
+  const LIGHTS_COUNT = 335;
   return (
     <div className='w-screen h-screen bg-sky-950 flex justify-center items-center flex-col'>
-      <div className='flex flex-wrap flex-row h-48 w-48 rounded-full opacity-50'>
-        {Array.from(Array(335)).map((_x, i) => {
-          const dimensionsIndex = randomIntInRange(0, DIMENSIONS.length);
-          const colorsIndex = randomIntInRange(0, COLORS.length);
+      <div className='flex flex-wrap flex-row h-48 w-48 rounded-full opacity-50 items-center justify-center'>
+        {Array.from(Array(LIGHTS_COUNT)).map((_x, i) => {
+          const dimensionsIndex = randomIntInRange(0, DIMENSIONS.length - 1);
+          const colorsIndex = randomIntInRange(0, COLORS.length - 1);
           return (
             <div
               className={cx(
