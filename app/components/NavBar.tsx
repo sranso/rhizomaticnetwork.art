71d495d383ab637eec1b0366cea32802bb62d779
Sarah Ransohoff <sranso@gmail.com>
2023-11-29 11:05:55 -0500

    Fix posit

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,8 +2,8 @@ import { Link } from '@remix-run/react';
 
 export default function NavBar() {
   return (
-    <nav className='fixed right-0 bottom-0 z-10 sm:m-3 flex justify-between bg-transparent'>
-      <Link to='/' className='text-green-600 rotate-180 text-xl font-extrabold'>
+    <nav className='fixed -right-20 bottom-24 z-10 flex justify-between bg-transparent'>
+      <Link to='/' className='text-green-600 rotate-90 text-xl font-extrabold'>
         Rhizomatic network
       </Link>
     </nav>
