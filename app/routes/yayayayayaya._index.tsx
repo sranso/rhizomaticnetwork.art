1706e8f3bee8132d153f68355e84bc1bc8392099
Sarah Ransohoff <sranso@gmail.com>
2023-11-30 12:47:29 -0500

    justify evenly, add mt

--- app/routes/yayayayayaya._index.tsx
+++ app/routes/yayayayayaya._index.tsx
@@ -1,6 +1,6 @@
 export default function Yayayayayaya() {
   return (
-    <div className='flex flex-col justify-around items-center overflow-hidden w-screen h-screen relative'>
+    <div className='flex flex-col justify-evenly items-center overflow-hidden w-screen h-screen relative'>
       <img
         src='https://media.tenor.com/iG6EyG57j_0AAAAC/davidbyrne-talkingheads.gif'
         alt='watercolor of leaves'
@@ -12,7 +12,7 @@ export default function Yayayayayaya() {
             Since each of us was several, there was already quite a crowd.
           </blockquote>
         </p>
-        <p className='animate-spin text-center'>
+        <p className='animate-spin text-center -mt-6'>
           <blockquote cite='https://files.libcom.org/files/A%20Thousand%20Plateaus.pdf'>
             Since each of us was several, there was already quite a crowd.
           </blockquote>
