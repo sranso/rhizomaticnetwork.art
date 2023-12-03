8fae626c4b561f62402cb6ddecad27c5b2acc36f
Sarah Ransohoff <sranso@gmail.com>
2023-12-03 10:54:05 -0500

    style lie down

--- app/routes/lie-down-with-me._index.tsx
+++ app/routes/lie-down-with-me._index.tsx
@@ -1,6 +1,6 @@
 export default function Start() {
   return (
-    <main className='w-10/12 md:w-8/12 mx-auto space-y-8 mt-4'>
+    <main className='w-10/12 md:w-6/12 mx-auto space-y-8 mt-4'>
       <p className='text-center text-sky-950'>
         One day Chao-chou fell down in the snow, and called out, "Help me up!
         Help me up!" A monk came and lay down beside him. Chao-chou got up and
