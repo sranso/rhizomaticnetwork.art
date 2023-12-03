3a224df558c7084d996838012df26456d07367fe
Sarah Ransohoff <sranso@gmail.com>
2023-12-03 10:53:11 -0500

    Style madeline

--- app/routes/madeline._index.tsx
+++ app/routes/madeline._index.tsx
@@ -1,8 +1,11 @@
 export default function IDKYet() {
   return (
-    <main className='flex'>
-      <img src='/madeline.gif' className='w-full absolute' />
-      <p className='w-10/12 md:w-2/5 mt-4 mx-auto text-center relative text-sky-950'>
+    <main className='flex md:flex-col-reverse'>
+      <img
+        src='/madeline.gif'
+        className='w-full absolute md:w-2/5 block mx-auto md:relative'
+      />
+      <p className='w-10/12 md:w-2/5 mt-4 mx-auto text-center relative text-sky-950 md:mb-4'>
         What is the color of wind?
         <br />
         (Koan)
